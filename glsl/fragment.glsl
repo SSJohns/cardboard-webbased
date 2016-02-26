e58cc2218786aa6b9a304cb3739d7c7ce4e38280
SSJohns <atstationy@AT-Station-Y.local>
2016-02-26 16:44:59 -0500

    raycasting to center of objects and updated verstion of three js

--- glsl/fragment.glsl
+++ glsl/fragment.glsl
@@ -1,5 +1,7 @@
-uniform vec2 resolution;
+varying vec3 vNormal;
 
-void main() {
-	gl_FragColor =
-	}
+void main()
+{
+	float intensity = pow( 0.7 - dot( vNormal, vec3( 0.0, 0.0, 1.0 ) ), 4.0 );
+    gl_FragColor = vec4( 1.0, 1.0, 1.0, 1.0 ) * intensity;
+}
--- glsl/vertex.glsl
+++ glsl/vertex.glsl
@@ -1,20 +1,7 @@
-varying float vUv;
+varying vec3 vNormal;
 
-void main() {
-  // modelViewMatrix, position and projectionMatrix are magical
-  // attributes that Three.js defines for us.
-
-  // Transform current vertex by the modelViewMatrix
-  // (bundled model world position & camera world position matrix).
-  vec4 mvPosition = modelViewMatrix * position;
-
-  // Project camera-space vertex to screen coordinates
-  // using the camera's projection matrix.
-  vec4 p = projectionMatrix * mvPosition;
-
-  // uv is another magical attribute from Three.js.
-  // We're passing it to the fragment shader unchanged.
-  vUv = uv;
-
-  gl_Position = p;
+void main()
+{
+    vNormal = normalize( normalMatrix * normal );
+    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
 }
